60bf0ea796a520e0fa76ce43503bd1b146b9186e
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-05-08 19:56:21 +0700

    membuat case 5 dan mengisinya

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -185,3 +185,15 @@ int main()
             }
         }
         break;
+        case '5':
+        {
+        }
+        break;
+        default:
+        {
+            cout << "Pilihan salah !." << endl;
+        }
+        break;
+    }
+    } while (ch != '5');
+}
